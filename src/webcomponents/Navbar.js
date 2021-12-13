3eb3f2d81aa88e38747b14d48af6bb6681fed0ba
belalelgebaly <belalelgebaly4@gmail.com>
2021-12-13 19:03:55 +0200

    navbar

--- src/webcomponents/Navbar.js
+++ src/webcomponents/Navbar.js
@@ -1,9 +1,19 @@
 const Navbar = () => {
     return ( 
         <nav className="navbar">
-            <h1>Navbar</h1>
+        <input type="checkbox" name="" id="check" />
+        <label for="check" className="checkbtn">
+            <i className="fas fa-bars"></i>
+        </label>
+      <ul>
+          <li><a href="/">HOME</a></li>
+          <li><a herf="#">ABOUT</a></li>
+          <li><a href="/sessions">PORTFOLIO</a></li>
+          <li><a href="/gettoknowus">CONTACT</a></li>
+      </ul>
         </nav>
-     );
+    );
 }
+ 
  
 export default Navbar;
