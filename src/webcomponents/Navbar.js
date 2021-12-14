8c585a9b10e70526252e633465a680d15205a564
belalelgebaly <belalelgebaly4@gmail.com>
2021-12-14 15:01:05 +0200

    section one added, plus edits to the navbar

--- src/webcomponents/Navbar.js
+++ src/webcomponents/Navbar.js
@@ -6,8 +6,8 @@ const Navbar = () => {
             <i className="fas fa-bars"></i>
         </label>
       <ul>
-          <li><a href="/">HOME</a></li>
-          <li><a herf="#">ABOUT</a></li>
+          <li><a href="/" style={{"color": "#D62139"}}>HOME</a></li>
+          <li><a herf="/">ABOUT</a></li>
           <li><a href="/sessions">PORTFOLIO</a></li>
           <li><a href="/gettoknowus">CONTACT</a></li>
       </ul>
--- src/webcomponents/Section1.js
+++ src/webcomponents/Section1.js
@@ -1,7 +1,10 @@
 const Section1 = () => {
     return ( 
         <section className="section1">
-            <h1>Section1</h1>
+            <div className="section1-items-container">
+                <div id="item1"><p>Hello, I’m <span>Belal Elgebaly</span>,<br></br> I’m a front-end web developer</p></div>
+                <div id="item2"><button>View my work</button></div>
+            </div>
         </section>
      );
 }
