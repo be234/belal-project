ffb33d3180b3c0f3800eb5fce0dd9d7f21bd05fd
belalelgebaly <belalelgebaly4@gmail.com>
2021-12-16 15:29:38 +0200

    fixed navbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 import './App.css';
 import Home from './Home';
+import './jscomponents/sticky.js';
 
 function App() {
-
-window.onscroll = function() {myFunction()};
-  
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
 
   return (
     <div className="App">
