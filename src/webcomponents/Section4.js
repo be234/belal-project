58db51972abe510fcd70547a78e0c8ffbd91a47a
belalelgebaly <belalelgebaly4@gmail.com>
2022-01-10 11:45:08 +0200

    contact section

--- src/webcomponents/Section4.js
+++ src/webcomponents/Section4.js
@@ -1,7 +1,23 @@
 const Section4 = () => {
     return ( 
         <section className="section4">
-            <h1>Section4</h1>
+            <div id="flag"></div>
+            <h1>CONTACT</h1>
+            <p>Have a question or want to work together?</p>
+            <form action="" className="contact-form">
+
+               <input type="text" id="name" name="name" placeholder="Name" required />
+               <input type="email" id="email" placeholder="Enter email" required />
+               <textarea
+                  id="commenting"
+                  className="textArea"
+                  name="comments" 
+                  placeholder="Your message"
+                  ></textarea>
+
+        <button type="submit" id="submit">SUBMIT</button> 
+
+            </form>
         </section>
      );
 }
