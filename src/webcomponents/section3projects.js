ec6a5eb76a0496a52e92406a961ac60fe69c7129
belalelgebaly <belalelgebaly4@gmail.com>
2022-01-19 15:24:48 +0200

    projects section window mobile responisve

---
+++ src/webcomponents/section3projects.js
@@ -0,0 +1,181 @@
+const Section3projects = () => {
+    return ( 
+        <div className="section3projects">
+                     
+{/* radio buttons */}
+            <input type="radio" id="toggle1" name="toggle" />
+            <input type="radio" id="toggle2" name="toggle" />
+            <input type="radio" id="toggle3" name="toggle" />
+            <input type="radio" id="toggle4" name="toggle" />
+            <input type="radio" id="toggle5" name="toggle" />
+            <input type="radio" id="toggle6" name="toggle" />
+            <input type="radio" id="toggle7" name="toggle" />
+            <input type="radio" id="toggle8" name="toggle" />
+            <input type="radio" id="toggle9" name="toggle" />
+
+{/* tab 1 */}
+        <span id="content1">
+          <div class="showed-div">
+            <img src="https://media.istockphoto.com/photos/coding-software-concept-developer-working-on-code-picture-id1284552053?k=20&m=1284552053&s=612x612&w=0&h=ri2L0d1uKo334hpVXpzQ45x67QALCb0GvFIkYWVqT78=" alt="project image" />
+            <h1>WalkerTracker</h1>
+            <h5>PERFORMANCE METRICS.</h5>
+            <hr />
+            <p>Walker Tracker offers goal management, fitness tracking, and team competitions to companies for internal use. A Ruby on Rails and Javascript companion site for the Walker Tracker App. Features visual metrics and gamified progression system.</p>
+            <a href="google.com" target="_blank" className="view-site-button"><div><span class="iconify" data-icon="mdi:open-in-new"></span></div> <span>VIEW SITE</span></a>
+            <label for="toggle0"><span class="iconify" data-icon="mdi:close"></span></label>
+            <input type="radio" id="toggle0" name="toggle" />
+
+          </div>
+          <label for="toggle0" id="close-tabs"></label>
+          <span id="content0"></span>
+          </span>
+
+{/* tab 2 */}
+          <span id="content2">
+          <div class="showed-div">
+            <img src="https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="project image" />
+            <h1>WalkerTracker</h1>
+            <h5>PERFORMANCE METRICS.</h5>
+            <hr />
+            <p>Walker Tracker offers goal management, fitness tracking, and team competitions to companies for internal use. A Ruby on Rails and Javascript companion site for the Walker Tracker App. Features visual metrics and gamified progression system.</p>
+            <input type="radio" id="toggle0" name="toggle" />
+            <a href="google.com" target="_blank" className="view-site-button"><div><span class="iconify" data-icon="mdi:open-in-new"></span></div> <span>VIEW SITE</span></a>
+            <label for="toggle0"><span className="iconify close-iconify" data-icon="mdi:close"></span></label>
+
+          </div>
+          <label for="toggle0" id="close-tabs"></label>
+          <span id="content0"></span>
+          </span>
+
+          
+{/* tab 3 */}
+<span id="content3">
+          <div class="showed-div">
+            <img src="https://images.unsplash.com/photo-1547032175-7fc8c7bd15b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="project image" />
+            <h1>WalkerTracker</h1>
+            <h5>PERFORMANCE METRICS.</h5>
+            <hr />
+            <p>Walker Tracker offers goal management, fitness tracking, and team competitions to companies for internal use. A Ruby on Rails and Javascript companion site for the Walker Tracker App. Features visual metrics and gamified progression system.</p>
+            <input type="radio" id="toggle0" name="toggle" />
+            <a href="google.com" target="_blank" className="view-site-button"><div><span class="iconify" data-icon="mdi:open-in-new"></span></div> <span>VIEW SITE</span></a>
+            <label for="toggle0"><span class="iconify" data-icon="mdi:close"></span></label>
+            
+          </div>
+          <label for="toggle0" id="close-tabs"></label>
+          <span id="content0"></span>
+          </span>
+
+          
+{/* tab 4 */}
+<span id="content4">
+          <div class="showed-div">
+            <img src="https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIwfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="project image" />
+            <h1>WalkerTracker</h1>
+            <h5>PERFORMANCE METRICS.</h5>
+            <hr />
+            <p>Walker Tracker offers goal management, fitness tracking, and team competitions to companies for internal use. A Ruby on Rails and Javascript companion site for the Walker Tracker App. Features visual metrics and gamified progression system.</p>
+            <input type="radio" id="toggle0" name="toggle" />
+            <a href="google.com" target="_blank" className="view-site-button"><div><span class="iconify" data-icon="mdi:open-in-new"></span></div> <span>VIEW SITE</span></a>
+            <label for="toggle0"><span class="iconify" data-icon="mdi:close"></span></label>
+            
+          </div>
+          <label for="toggle0" id="close-tabs"></label>
+          <span id="content0"></span>
+          </span>
+
+
+          
+{/* tab 5 */}
+<span id="content5">
+          <div class="showed-div">
+            <img src="https://images.unsplash.com/photo-1559526324-593bc073d938?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="project image" />
+            <h1>WalkerTracker</h1>
+            <h5>PERFORMANCE METRICS.</h5>
+            <hr />
+            <p>Walker Tracker offers goal management, fitness tracking, and team competitions to companies for internal use. A Ruby on Rails and Javascript companion site for the Walker Tracker App. Features visual metrics and gamified progression system.</p>
+            <input type="radio" id="toggle0" name="toggle" />
+            <a href="google.com" target="_blank" className="view-site-button"><div><span class="iconify" data-icon="mdi:open-in-new"></span></div> <span>VIEW SITE</span></a>
+            <label for="toggle0"><span class="iconify" data-icon="mdi:close"></span></label>
+            
+          </div>
+          <label for="toggle0" id="close-tabs"></label>
+          <span id="content0"></span>
+          </span>
+
+          
+{/* tab 6 */}
+<span id="content6">
+          <div class="showed-div">
+            <img src="https://images.unsplash.com/photo-1591696205602-2f950c417cb9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="project image" />
+            <h1>WalkerTracker</h1>
+            <h5>PERFORMANCE METRICS.</h5>
+            <hr />
+            <p>Walker Tracker offers goal management, fitness tracking, and team competitions to companies for internal use. A Ruby on Rails and Javascript companion site for the Walker Tracker App. Features visual metrics and gamified progression system.</p>
+            <input type="radio" id="toggle0" name="toggle" />
+            <a href="google.com" target="_blank" className="view-site-button"><div><span class="iconify" data-icon="mdi:open-in-new"></span></div> <span>VIEW SITE</span></a>
+            <label for="toggle0"><span class="iconify" data-icon="mdi:close"></span></label>
+            
+          </div>
+          <label for="toggle0" id="close-tabs"></label>
+          <span id="content0"></span>
+          </span>
+
+          
+{/* tab 7 */}
+<span id="content7">
+          <div class="showed-div">
+            <img src="https://images.unsplash.com/photo-1563986768711-b3bde3dc821e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1168&q=80" alt="project image" />
+            <h1>WalkerTracker</h1>
+            <h5>PERFORMANCE METRICS.</h5>
+            <hr />
+            <p>Walker Tracker offers goal management, fitness tracking, and team competitions to companies for internal use. A Ruby on Rails and Javascript companion site for the Walker Tracker App. Features visual metrics and gamified progression system.</p>
+            <input type="radio" id="toggle0" name="toggle" />
+            <a href="google.com" target="_blank" className="view-site-button"><div><span class="iconify" data-icon="mdi:open-in-new"></span></div> <span>VIEW SITE</span></a>
+            <label for="toggle0"><span class="iconify" data-icon="mdi:close"></span></label>
+            
+          </div>
+          <label for="toggle0" id="close-tabs"></label>
+          <span id="content0"></span>
+          </span>
+
+          
+{/* tab 8 */}
+<span id="content8">
+          <div class="showed-div">
+            <img src="https://images.unsplash.com/photo-1460925895917-afdab827c52f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1115&q=80" alt="project image" />
+            <h1>WalkerTracker</h1>
+            <h5>PERFORMANCE METRICS.</h5>
+            <hr />
+            <p>Walker Tracker offers goal management, fitness tracking, and team competitions to companies for internal use. A Ruby on Rails and Javascript companion site for the Walker Tracker App. Features visual metrics and gamified progression system.</p>
+            <input type="radio" id="toggle0" name="toggle" />
+            <a href="google.com" target="_blank" className="view-site-button"><div><span class="iconify" data-icon="mdi:open-in-new"></span></div> <span>VIEW SITE</span></a>
+            <label for="toggle0"><span class="iconify" data-icon="mdi:close"></span></label>
+            
+          </div>
+          <label for="toggle0" id="close-tabs"></label>
+          <span id="content0"></span>
+          </span>
+
+          
+{/* tab 9 */}
+<span id="content9">
+          <div class="showed-div">
+            <img src="https://images.unsplash.com/photo-1526628953301-3e589a6a8b74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEwfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="project image" />
+            <h1>WalkerTracker</h1>
+            <h5>PERFORMANCE METRICS.</h5>
+            <hr />
+            <p>Walker Tracker offers goal management, fitness tracking, and team competitions to companies for internal use. A Ruby on Rails and Javascript companion site for the Walker Tracker App. Features visual metrics and gamified progression system.</p>
+            <input type="radio" id="toggle0" name="toggle" />
+            <a href="google.com" target="_blank" className="view-site-button"><div><span class="iconify" data-icon="mdi:open-in-new"></span></div> <span>VIEW SITE</span></a>
+            <label for="toggle0"><span class="iconify" data-icon="mdi:close"></span></label>
+            
+          </div>
+          <label for="toggle0" id="close-tabs"></label>
+          <span id="content0"></span>
+          </span>
+
+        </div>
+     );
+}
+ 
+export default Section3projects;
