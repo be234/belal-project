6bccf52df338680ef1f09ac61800f3188c9480ec
belalelgebaly <belalelgebaly4@gmail.com>
2022-01-27 14:21:26 +0200

    email

--- src/webcomponents/Section4.js
+++ src/webcomponents/Section4.js
@@ -9,7 +9,7 @@ const Section4 = () => {
             <p  data-aos="fade-up" data-aos-duration="800" data-aos-once="true">Have a question or want to work together?</p>
 
      <div data-aos="fade-up" data-aos-duration="800" data-aos-once="true">
-            <form action="https://formsubmit.co/belalelgebaly20000@gmail.com" method="POST" className="contact-form">
+            <form action="https://formsubmit.co/belalelgebalymoi@gmail.com" method="POST" className="contact-form">
               <input type="hidden" name="_captcha" value="false" />
                <input type="text" id="name" name="name" placeholder="Name" required />
                <input type="email" name="email" id="email" placeholder="Enter email" required />
