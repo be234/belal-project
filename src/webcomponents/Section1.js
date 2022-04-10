4d30730779c6b355680f584005469da92883cc41
belalelgebaly <belalelgebaly4@gmail.com>
2022-04-10 03:35:57 +0200

    mostly over

--- src/webcomponents/Section1.js
+++ src/webcomponents/Section1.js
@@ -4,7 +4,7 @@ const Section1 = () => {
                         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css"></link>
             <div className="section1-items-container">
                 <div id="item1"><p>Hello, I’m <span class="highlight">Belal Elgebaly</span>,<br></br> I’m a front-end web developer</p></div>
-                <div id="item2"><form action="#section2"><button href="#section2">View my work <span id="arrow" class="iconify" data-icon="mdi:arrow-right"></span> </button></form></div>
+                <div id="item2"><a href="#section22" className="button">View my work <span id="arrow" class="iconify" data-icon="mdi:arrow-right"></span> </a></div>
             </div>
         </section>
      );
